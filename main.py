1eff2bd4996917707ac772b8b440258fe4d8c1ec
Aleksei Savinov <mcsava@MacBook-Pro-Aleksei.local>
2024-12-05 19:50:27 +0300

    files

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv('annotation.csv')
+
+st.title('10 Рандомных фильмов')
+
+if st.button('Жмай'):
+    random = df[['author', 'title']].sample(n=10)
+    st.write(random)
+
+
